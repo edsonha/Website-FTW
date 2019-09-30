5f80038ac3d2cb636694ad5a8205a3998a97c915
edsonha <edsonha@gmail.com>
2019-09-30 21:46:09 +0800

    Second website commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color1: "#9fb8ad",
+      color2: "#1fc8db",
+      color3: "#2cb5e8"
+    }
+  }
+
+  setBackgroundColor = () => {
+    this.setState({
+      color1: "#" + Math.floor(Math.random()*16777215).toString(16),
+      color2: "#" + Math.floor(Math.random()*16777215).toString(16),
+      color3: "#" + Math.floor(Math.random()*16777215).toString(16),
+    })
+  }
+
+  render () {
+    const { color1, color2, color3 } = this.state;
+    return (
+      <div className="container" style={{background: `linear-gradient(141deg, ${color1}, ${color2} 51%, ${color3} 75%)`}}>
+        <div className="center">
+          <h1>RANDOM GRADIENT BACKGROUND GENERATOR</h1>
+          <h3>Current CSS background:</h3>
+          <p>linear-gradient(141deg, {color1}, {color2} 51%, {color3} 75%)</p>
+          <button onClick={this.setBackgroundColor}>Click Me!</button>
+        </div>
+      </div>
+    );
+  }
+} 
 
 export default App;
