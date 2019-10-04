7cb8fbbd1f4d76733492143d046d2a57a714aa92
edsonha <edsonha@gmail.com>
2019-10-04 19:55:38 +0800

    Finish paint-a-mondrian project

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ class App extends  Component {
   constructor(props) {
     super(props);
     this.state = {
-      color: "",
-      backgroundColor: ""
+      color: ''
     }
   }
 
-  setColor = (input) => {
-    this.setState({color: input});
+  setColor = (colorInput) => {
+    this.setState({color: colorInput})
   }
 
-  setBackgroundColor = (input2) => {
-    this.setState({backgroundColor: input2})
+  paintBox = (colorInput, boxInput) => {
+    let boxToPaint = document.getElementById(boxInput);
+    boxToPaint.style.backgroundColor = colorInput;
+  }
+
+  reset = () => {
+    let resetBox = document.getElementsByClassName("zone");
+    for (let box of resetBox) {
+      box.style.backgroundColor = "white"
+    }
   }
 
   render() {
@@ -28,28 +35,27 @@ class App extends  Component {
             <div className="color" id="yellow" onClick={() => this.setColor("#ffec00")}></div>
             <div className="color" id="blue" onClick={() => this.setColor("#0000cc")}></div>
             <div className="color" id="white" onClick={() => this.setColor("white")}></div>
+            <button onClick={this.reset}>Reset</button>
           </div>
-          <div className="canvas">
-            <div className="container">
-              <div className="zone one" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone one" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone one" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone one" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone two" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone two" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone two" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone two" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone three" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone three" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone three" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone three" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}} ></div>
-              <div className="zone four" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone four" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone four" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-              <div className="zone four" onClick={() => this.setBackgroundColor(this.state.color)} style={{backgroundColor: this.state.backgroundColor}}></div>
-            </div>
+          <div id="canvas">
+            <div className="zone one" id="box-1" onClick={() => this.paintBox(this.state.color, "box-1")}></div>
+            <div className="zone" id="box-2" onClick={() => this.paintBox(this.state.color, "box-2")}></div>
+            <div className="zone" id="box-3" onClick={() => this.paintBox(this.state.color, "box-3")}></div>
+            <div className="zone" id="box-4" onClick={() => this.paintBox(this.state.color, "box-4")}></div>
+            <div className="zone two" id="box-5" onClick={() => this.paintBox(this.state.color, "box-5")}></div>
+            <div className="zone" id="box-6" onClick={() => this.paintBox(this.state.color, "box-6")}></div>
+            <div className="zone" id="box-7" onClick={() => this.paintBox(this.state.color, "box-7")}></div>
+            <div className="zone" id="box-8" onClick={() => this.paintBox(this.state.color, "box-8")}></div>
+            <div className="zone three" id="box-9" onClick={() => this.paintBox(this.state.color, "box-9")}></div>
+            <div className="zone" id="box-10" onClick={() => this.paintBox(this.state.color, "box-10")}></div>
+            <div className="zone" id="box-11" onClick={() => this.paintBox(this.state.color, "box-11")}></div>
+            <div className="zone" id="box-12" onClick={() => this.paintBox(this.state.color, "box-12")}></div>
+            <div className="zone four" id="box-13" onClick={() => this.paintBox(this.state.color, "box-13")}></div>
+            <div className="zone" id="box-14" onClick={() => this.paintBox(this.state.color, "box-14")}></div>
+            <div className="zone" id="box-15" onClick={() => this.paintBox(this.state.color, "box-15")}></div>
+            <div className="zone" id="box-16" onClick={() => this.paintBox(this.state.color, "box-16")}></div>
           </div>
-          </div>
+        </div>
       </div>
     )
   }
