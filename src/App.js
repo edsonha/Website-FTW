60bfe728bb5ed99f879f54fba1b68816881ea5ad
edsonha <edsonha@gmail.com>
2019-10-05 16:02:09 +0800

    Finish technicolor box project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const boxesId = [...Array(24).keys()]
+
+  const setTechnicolor = (boxInput) => {
+    const randomColor = () => '#' + Math.random().toString(16).slice(2,8);
+    const boxToColor = document.getElementById(boxInput);
+    boxToColor.style.backgroundColor = randomColor();
+    boxToColor.style.boxShadow = '0 0 8px white'
+    boxToColor.style.zIndex = 100
+  }
+
+  const leaveBox = (boxInput) => {
+    const boxToLeave = document.getElementById(boxInput);
+    boxToLeave.style.boxShadow = 'none';
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Technicolor Box</h1>
+      <div className="container">
+        {boxesId.map((boxId) => {
+          return (
+            <div key={boxId} className="box" id={boxId} onMouseEnter={() => setTechnicolor(boxId)} onMouseLeave={() => leaveBox(boxId) }>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
