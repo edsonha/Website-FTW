e5f6a6d6b579b14afde845e75b4b8ad867a01372
edsonha <edsonha@gmail.com>
2020-01-23 23:22:37 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
-# Website-FTW
+# Apple Pie or Choco Bar
 
-Inspired by #100DaysOfCode and Jennifer Dewalt's challenge: 180 websites in 180 days, here is a collection of small projects to practice my existing and new skills in software development.
+Coming Soon -- Displaying pie chart and bar chart using React and Recharts
 
-## Project Source Code
+## Created Using
 
-- Project 1: [Random-Background-Generator](https://github.com/edsonha/Website-FTW/tree/random-background-generator)
-- Project 2: [Paint-A-Mondrian](https://github.com/edsonha/Website-FTW/tree/paint-a-mondrian)
-- Project 3: [Technicolor-Box](https://github.com/edsonha/Website-FTW/tree/technicolor-box)
-- Project 4: [Scissors-Paper-Stone](https://github.com/edsonha/Website-FTW/tree/scissors-paper-stone)
-- Project 5: [Robofriends-Typescript](https://github.com/edsonha/Website-FTW/tree/robofriends-typescript)
-- Project 6: [Tic-Tac-Toe](https://github.com/edsonha/Website-FTW/tree/tic-tac-toe)
-- Project 7: [Todo-List](https://github.com/edsonha/Website-FTW/tree/todo-list)
+- React
+- Recharts
+- Bootstrap
+
+## Screenshot
+
+Coming Soon
--- src/App.js
+++ src/App.js
@@ -1,26 +1,236 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  LineChart,
+  Line
+} from "recharts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const data = [
+  {
+    name: "Page A",
+    // uv: 4000,
+    // pv: 2400,
+    amt: 1000
+  },
+  {
+    name: "Page B",
+    // uv: 3000,
+    // pv: 1398,
+    amt: 1500
+  },
+  {
+    name: "Page C",
+    // uv: 2000,
+    // pv: 9800,
+    amt: 2500
+  },
+  {
+    name: "Page D",
+    // uv: 2780,
+    // pv: 3908,
+    amt: 2000
+  },
+  {
+    name: "Page E",
+    // uv: 1890,
+    // pv: 4800,
+    amt: 800
+  }
+];
+
+const options = [
+  { name: "Bar Chart", value: "bar" },
+  { name: "Pie Chart", value: "pie" }
+];
+
+class App extends Component {
+  state = {
+    selectedChart: "bar",
+    nameInput: "",
+    amountInput: 0,
+    data: data
+  };
+
+  handleSelectChart = event => {
+    this.setState({ selectedChart: event.target.value });
+  };
+
+  handleNameInput = event => {
+    this.setState({ nameInput: event.target.value });
+  };
+
+  handleAmountInput = event => {
+    this.setState({ amountInput: event.target.value });
+  };
+
+  handleCreateData = () => {
+    const copiedData = [...this.state.data];
+    copiedData.push({
+      name: this.state.nameInput,
+      amt: this.state.amountInput
+    });
+    this.setState({
+      data: copiedData
+    });
+  };
+
+  handleDeleteData = () => {
+    const copiedData = [...this.state.data];
+    const filteredData = copiedData.filter(
+      data => data.name !== this.state.nameInput
+    );
+    this.setState({
+      data: filteredData
+    });
+  };
+
+  handleEditData = () => {
+    const copiedData = [...this.state.data];
+    const foundData = copiedData.find(
+      data => data.name === this.state.nameInput
+    );
+    foundData.amt = this.state.amountInput;
+    this.setState({ data: copiedData });
+  };
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="sort-by-select">Choose Chart</label>
+        <select
+          onChange={this.handleSelectChart}
+          id="sort-by-select"
+          data-testid="sort-by-select"
+          className="btn btn-primary dropdown-toggle mx-2"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {options.map((option, index) => (
+            <option key={index} value={option.value}>
+              {option.name}
+            </option>
+          ))}
+        </select>
+        <div className="row">
+          <div className="input-group mb-3 col-lg-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter name..."
+              onChange={this.handleNameInput}
+            />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter amount..."
+              onChange={this.handleAmountInput}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={this.handleCreateData}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-group mb-3 col-lg-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Delete name..."
+              onChange={this.handleNameInput}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-danger"
+                type="button"
+                onClick={this.handleDeleteData}
+              >
+                -
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-group mb-3 col-lg-4">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter name..."
+              onChange={this.handleNameInput}
+            />
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Editted amount..."
+              onChange={this.handleAmountInput}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-success"
+                type="button"
+                onClick={this.handleEditData}
+              >
+                +
+              </button>
+            </div>
+          </div>
+        </div>
+        {this.state.selectedChart === "bar" ? (
+          <BarChart
+            width={1000}
+            height={500}
+            data={this.state.data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="amt" fill="#8884d8" />
+          </BarChart>
+        ) : (
+          <LineChart
+            width={1000}
+            height={500}
+            data={this.state.data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="amt"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
