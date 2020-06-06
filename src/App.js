d85fdf6f625d63197dd8062cecaa5976c02532c2
edsonha <edsonha@gmail.com>
2020-06-06 09:48:53 +0800

    Commit base project to be user for code-splitting work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Page1 from "./components/Page1.jsx";
+import Page2 from "./components/Page2.jsx";
+import Page3 from "./components/Page3.jsx";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      route: "page1",
+    };
+  }
+
+  onRouteChange = (route) => {
+    this.setState({ route });
+  };
+
+  render() {
+    switch (this.state.route) {
+      case "page2":
+        return <Page2 onRouteChange={this.onRouteChange} />;
+      case "page3":
+        return <Page3 onRouteChange={this.onRouteChange} />;
+      default:
+        return <Page1 onRouteChange={this.onRouteChange} />;
+    }
+  }
 }
 
 export default App;
---
+++ src/components/Page2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
+
+const Page2 = ({ onRouteChange }) => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <button onClick={() => onRouteChange("page1")}>Page 1</button>
+      <button className="disabled">Page 2</button>
+      <button onClick={() => onRouteChange("page3")}>Page 3</button>
+    </header>
+  </div>
+);
+
+export default Page2;
