03a871a4dc6448bd6c6e36de321847c99e8a37ae
edsonha <edsonha@gmail.com>
2020-06-06 10:06:30 +0800

    Implement 1st code-splitting - manual way

--- src/App.js
+++ src/App.js
@@ -2,29 +2,63 @@ import React from "react";
 import "./App.css";
 
 import Page1 from "./components/Page1.jsx";
-import Page2 from "./components/Page2.jsx";
-import Page3 from "./components/Page3.jsx";
+// Part 2 - Code Splitting - manual
+// import Page2 from "./components/Page2.jsx";
+// import Page3 from "./components/Page3.jsx";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       route: "page1",
+
+      // Part 2 - Code Splitting - manual
+      component: null,
     };
   }
 
   onRouteChange = (route) => {
-    this.setState({ route });
+    // Part 1 - No Code Splitting
+    // this.setState({ route });
+
+    // Part 2 - Code Splitting - manual
+    if (route === "page1") {
+      this.setState({ route: route });
+    } else if (route === "page2") {
+      import("./components/Page2")
+        .then((Page2) => {
+          this.setState({ route: route, component: Page2.default });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      import("./components/Page3")
+        .then((Page3) => {
+          this.setState({ route: route, component: Page3.default });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   render() {
-    switch (this.state.route) {
-      case "page2":
-        return <Page2 onRouteChange={this.onRouteChange} />;
-      case "page3":
-        return <Page3 onRouteChange={this.onRouteChange} />;
-      default:
-        return <Page1 onRouteChange={this.onRouteChange} />;
+    // Part 1 - No code splitting
+    // switch (this.state.route) {
+    //   case "page2":
+    //     return <Page2 onRouteChange={this.onRouteChange} />;
+    //   case "page3":
+    //     return <Page3 onRouteChange={this.onRouteChange} />;
+    //   default:
+    //     return <Page1 onRouteChange={this.onRouteChange} />;
+    // }
+
+    // Part 2 - No Code Splitting - manual
+    if (this.state.route === "page1") {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    } else {
+      return <this.state.component onRouteChange={this.onRouteChange} />;
     }
   }
 }
